d03cfc78a7e3a2525cc43e9109310bbf114ebabb
wis-ngops <109586766+wis-ngops@users.noreply.github.com>
2022-09-02 11:38:20 +0700

    Add files via upload

---
+++ styling2.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+  margin:2,
+  loop:true,
+  autoWidth:true,
+  nav:true,
+  autoplay:true,
+  responsiveClass:true,
+  autoplayTimeout:4000,
+})
+
+$('#datepicker').datepicker('show');
+
